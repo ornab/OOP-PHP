69005da18d8e70295302c24dd88650cadf7e3ade
ornab <hzamil17@gmail.com>
2017-01-12 20:14:58 +0600

    modified

--- inheritance.php
+++ inheritance.php
@@ -1,5 +1,6 @@
 <?php
 
+/************ Counting Eye ************/
 //class Mother
 //{
 //    public function getEyeCount()
@@ -16,51 +17,115 @@
 //    
 //}
 //
-//(new Child)->getEyeCount();
+//echo (new Child)->getEyeCount();
 
-class Shape
-    
+
+///************ Counting Different Shapes ************/
+//
+//abstract class Shape
+//    
+//{
+//   protected $color;
+//    
+//    public function __construct($color = 'red ')
+//    {
+//       $this->color = $color;
+//        
+//    }
+//    
+//    public function getColor()
+//    {
+//        
+//        return $this->color;
+//        
+//    }
+//    
+//    abstract protected function getArea(); 
+//    
+//}
+//
+//class Square extends Shape
+//
+//{
+//    protected $length = 4;
+//    public function getArea()
+//   
+//   {
+//       return pow($this->length, 2);  
+//       
+//       
+//   } 
+//    
+//    
+//}
+//
+//
+//class Triangle extends Shape
+//
+//{
+//    protected $base = 4;
+//    protected $height = 7;
+//    public function getArea()
+//   
+//   {
+//       return 0.5 * $this->base * $this->height ;  
+//       
+//       
+//   }
+//    
+//    
+//    
+//}
+//
+//class Circle extends Shape
+//{
+//    protected $radius = 5;
+//    
+//    public function getArea()
+//    {
+//        
+//        return M_PI * pow($this->radius, 2);
+//        
+//    }
+//    
+//    
+//}
+//
+// //ShortHand
+//echo "Square: " . (new Square)->getColor() . '<br> <br>';
+//    
+//echo "Triangle: " . (new Triangle)->getArea() . '<br> <br>';
+//
+////LongHand
+//
+//$circle = new Circle();
+//echo "Circle: " . $circle->getArea();
+
+/*************** Mailer ***************/
+
+class Mailer
 {
-    protected $length = 4;
-    public function getArea()
-   
-   {
-       return pow($this->length, 2);  
-       
-       
-   } 
+    public function send($to, $from, $body)
+    {
+        
+        
+    }
     
 }
 
-class Square extends Shape
-
+class UserMailer extends Mailer
 {
-    
-    
-}
-
-
-class Triangle extends Shape
-
-{
-    protected $base = 4;
-    protected $height = 7;
-    public function getArea()
-   
-   {
-       return 0.5 * $this->base * $this->height ;  
-       
-       
-   }
     
     
     
 }
 
- 
-echo "Square:" . (new Square)->getArea() . '<br> <br>';
+class AdminMailer extends Mailer
+{
     
-echo "Triangle:" . (new Triangle)->getArea();
+    
+    
+}
 
 
 
