536678661540efd0d812cd4fd25a5e941b056a27
ornab <hzamil17@gmail.com>
2017-01-12 19:17:40 +0600

    initial commit

---
+++ encapsulation.php
@@ -0,0 +1,45 @@
+<?php
+
+class Person
+{
+    private $name;  //Private properties can't be acceessed from outside the class
+    
+    private $age;
+    
+    public function __construct($name)
+    {
+        
+        $this->name = $name;
+        
+    }
+    
+    public function getAge()
+    {
+        
+        return $this->age;
+        
+    }
+    
+    public function setAge($age)
+        
+    {
+        if($age<18)
+        {
+            
+            throw new Exception("You are younger than 18!");
+            
+        }
+        
+        $this->age = $age;
+        
+    }
+    
+}
+
+$john = new Person('John Doe');
+
+$john->setAge(20);
+
+ 
+var_dump($john->getAge());
+
